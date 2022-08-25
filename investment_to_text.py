fee2212ed0a890fdbf3e042b6e8c8efaf41547fa
Anders <andersbandt@yahoo.com>
2022-08-25 15:27:18 -0500

    Update investment_to_text.py
    Cleaned up some stuff again based on new coding knowledge :)

--- investment_to_text.py
+++ investment_to_text.py
@@ -7,8 +7,19 @@ import json
 import smtplib
 from email.mime.text import MIMEText
 
-# get stock information
+# user needed information to run program
 ticker = "Your stock ticker"
+buy_price = 0 # price for purchasing the stock
+
+me = "your email here"
+to = "your phone number"
+to = to + "@vtext.com" # NOTE: I think this only works for Verizon phone numbers
+login = "your gmail address"
+password = "your password"
+smptserver = "smtp.gmail.com:587" # most likely don't need to change
+
+
+# get stock information
 info = getQuotes(ticker) # For example: "AAPL", "MSFT", etc.
 stuff = json.dumps(info)
 final = ""
@@ -17,34 +28,25 @@ for i in range(118,123):
 price = float(final)
 
 value = price * 128.514
-totalnet = value - 3200
+totalnet = value - buy_price
+
 
 # set up some ranges (in dollars) to change your message
 net_low_range = -300
-net_small_high_range = 50
 net_high_range = 300
+
 
 # conditionals to form message based on net gain
 if totalnet < net_low_range:
 	message = "Hmm.. things are looking rough. Your net loss is %s" % totalnet
-
 elif totalnet < 0:
 	message = "At least you aren't dead! Your total net loss is %s" % totalnet
-
-elif totalnet > 0 and totalnet < 50:
+elif totalnet > 0 and totalnet < net_high_range:
 	message = "At least you're above 0! Your total net gain is %s" % totalnet
-
-elif totalnet > 300:
+elif totalnet > net_high_range:
 	message = "Wow you're doing amazing! Your total net gain is %s" % totalnet
 
 print(message)
-
-me = "your email here"
-to = "your phone number"
-to = to + "@vtext.com"
-login = "your gmail address"
-password = "your password"
-smptserver = "smtp.gmail.com:587"
 
 # create send email funcion
 def sendemail(sender, to, message, login, password, smptserver):
