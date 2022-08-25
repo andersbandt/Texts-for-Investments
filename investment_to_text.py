312ce20f00de308e799698f28751346ec7910cb6
Anders <andersbandt@yahoo.com>
2022-08-25 15:24:38 -0500

    Update investment_to_text.py
    Cleaned up some stuff with my new knowledge on clean coding :)

--- investment_to_text.py
+++ investment_to_text.py
@@ -1,55 +1,59 @@
 # Texts-for-Investments
-Sends a phone number investment data
+# PROGRAM DESCRIPTION: Sends a phone number investment data
+# 	A good use case might be running this on a server once a day to get stock updates.
 
 from googlefinance import getQuotes
 import json
 import smtplib
 from email.mime.text import MIMEText
 
-
-info = getQuotes("Your stock ticker") # For example: "AAPL", "MSFT", etc.
-
+# get stock information
+ticker = "Your stock ticker"
+info = getQuotes(ticker) # For example: "AAPL", "MSFT", etc.
 stuff = json.dumps(info)
-
 final = ""
-
 for i in range(118,123):
 	 final += stuff[i]
-
 price = float(final)
 
 value = price * 128.514
 totalnet = value - 3200
 
-if totalnet < -300:
+# set up some ranges (in dollars) to change your message
+net_low_range = -300
+net_small_high_range = 50
+net_high_range = 300
+
+# conditionals to form message based on net gain
+if totalnet < net_low_range:
 	message = "Hmm.. things are looking rough. Your net loss is %s" % totalnet
 
-if totalnet < 0:
-	"At least you aren't dead! Your total net loss is %s" % totalnet
+elif totalnet < 0:
+	message = "At least you aren't dead! Your total net loss is %s" % totalnet
 
-if totalnet > 0 and totalnet < 50:
-	"At least you're above 0! Your total net gain is %s" % totalnet
+elif totalnet > 0 and totalnet < 50:
+	message = "At least you're above 0! Your total net gain is %s" % totalnet
 
-if totalnet > 300:
-	"Wow you're doing amazing! Your total net gain is %s" % totalnet
+elif totalnet > 300:
+	message = "Wow you're doing amazing! Your total net gain is %s" % totalnet
 
-print message
+print(message)
 
 me = "your email here"
-to = "your phone number"@vtext.com"
+to = "your phone number"
+to = to + "@vtext.com"
 login = "your gmail address"
 password = "your password"
 smptserver = "smtp.gmail.com:587"
 
-
-def sendemail(sender, to, message, login,
-	password, smptserver):
-
+# create send email funcion
+def sendemail(sender, to, message, login, password, smptserver):
 	server = smtplib.SMTP(smptserver)
 	server.starttls()
 	server.login(login,password)
 	problems = server.sendmail(sender, to, message)
 
+# send the email
 sendemail(me, to, message, login, password, smptserver)
 
 
